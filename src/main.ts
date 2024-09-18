32297d18b4a8259a5a7195a06d0f06177fb1c360
juliogmo <20200775@uthh.edu.mx>
2024-09-18 13:42:01 -0600

    primer comit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config'; // Asegúrate de que el archivo esté correctamente referenciado
+import { AppComponent } from './app/app.component'; // Importa tu componente principal
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
